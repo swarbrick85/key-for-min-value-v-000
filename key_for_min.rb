b4a728e5260c2620528f3ce1b96c4d04d5f23be7
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-15 18:37:58 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+y = []
+if name_hash.size == 0 
+  return nil 
+end 
+name_hash.collect do |k, v| 
+  y<<v 
+end 
+while y.size > 1 
+if y[0] > y [1]
+  y.delete(0)
+end 
+if y[0] < y [1] 
+  y.delete(1) 
 end
+end 
+a = y.to_s
+return name_hash.index(a)
+end  
